2e494237db24757d2dbe3dff8d831bdf7ce3ad2f
AlaaKhaledAhmed <97679146+AlaaKhaledAhmed@users.noreply.github.com>
2022-03-21 08:44:21 +0300

    alaa

--- lib/Models/Variables.dart
+++ lib/Models/Variables.dart
@@ -10,7 +10,7 @@ const Color yellow = Color(0xFFECBB5C);
 const Color grey = Color(0xFF323435);
 const Color lightBlack = Color(0xFF171616); ///Used in texts fields and container
 const Color lightPurple = Color(0xFF7B7890);
-const Color cardColor = Color(0xFF0D0D0D); ///Used in suTitle in introduction screen
+const Color cardColor = Color(0xFF0D0D0D);
 const Color transparent = Colors.transparent;
 
 ///Icons
