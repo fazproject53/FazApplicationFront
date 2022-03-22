d089f2afdf75d420569808ec3e4a7cfc0e97080c
fazproject53 <fazproject53@gmail.com>
2022-03-22 10:48:45 +0300

    intro

--- lib/home/IntroductionScreen/Intro2.dart
+++ lib/home/IntroductionScreen/Intro2.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+
+import '../../Models/Methods.dart';
+import '../../Models/Variables.dart';
 class Intro2 extends StatefulWidget {
   const Intro2({Key? key}) : super(key: key);
 
@@ -11,7 +14,23 @@ class _Intro2State extends State<Intro2> {
   Widget build(BuildContext context) {
 
     return Scaffold(
-      backgroundColor: Colors.green,
-    );
+        backgroundColor: blackBackground,
+        body: SizedBox(
+          height: double.infinity,
+          width: double.infinity,
+          child: Column(
+            children: [
+              Expanded(flex: 2, child: ListTile(
+                title:  text(context, "واقع ملموس", 32, white,fontWeight: FontWeight.bold),
+                subtitle:text(context, "يمكننا تحويل فكرتك الى واقع ملموس", 16, lightPurple),
+              )),
+              Spacer(),
+              const Expanded(
+                child: Image(image: AssetImage("assets/image/int2.png"),fit: BoxFit.contain,),
+                flex: 8,
+              )
+            ],
+          ),
+        ));
   }
 }
