65e652cf293986301230a8074c52ed8cf2961288
fazproject53 <fazproject53@gmail.com>
2022-04-04 13:10:50 +0300

    get API request

--- lib/HomeScreen-Main/Packages/aleart.dart
+++ lib/HomeScreen-Main/Packages/aleart.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 
 show(context) {
   return showDialog(
-
+      barrierDismissible: false,
       context: context,
       builder: (context) {
         return AlertDialog(
-          backgroundColor: Colors.transparent,
+         // backgroundColor: Colors.transparent,
           title: Text("انتظررررررر من فضلك"),
           actions: [IconButton(onPressed: () {
-
+          Navigator.pop(context);
           }, icon: Icon(Icons.delete))],
           content: Container(
             child: CircularProgressIndicator(),
